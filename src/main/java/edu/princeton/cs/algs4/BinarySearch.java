d76e57fdef957cfd23842f6ec366775b9f8bafdc
windhua <1724347606@qq.com>
2020-12-07 15:13:44 +0800

    1.1.1二分法

--- src/main/java/edu/princeton/cs/algs4/BinarySearch.java
+++ src/main/java/edu/princeton/cs/algs4/BinarySearch.java
@@ -1,56 +1,61 @@
-/******************************************************************************
- *  Compilation:  javac BinarySearch.java
- *  Execution:    java BinarySearch allowlist.txt < input.txt
- *  Dependencies: In.java StdIn.java StdOut.java
- *  Data files:   https://algs4.cs.princeton.edu/11model/tinyW.txt
- *                https://algs4.cs.princeton.edu/11model/tinyT.txt
- *                https://algs4.cs.princeton.edu/11model/largeW.txt
- *                https://algs4.cs.princeton.edu/11model/largeT.txt
- *
- *  % java BinarySearch tinyW.txt < tinyT.txt
- *  50
- *  99
- *  13
- *
- *  % java BinarySearch largeW.txt < largeT.txt | more
- *  499569
- *  984875
- *  295754
- *  207807
- *  140925
- *  161828
- *  [367,966 total values]
- *  
- ******************************************************************************/
-
+/**
+ * Compilation:  javac BinarySearch.java
+ * Execution:    java BinarySearch allowlist.txt < input.txt
+ * Dependencies: In.java StdIn.java StdOut.java
+ * Data files:   https://algs4.cs.princeton.edu/11model/tinyW.txt
+ * https://algs4.cs.princeton.edu/11model/tinyT.txt
+ * https://algs4.cs.princeton.edu/11model/largeW.txt
+ * https://algs4.cs.princeton.edu/11model/largeT.txt
+ * <p>
+ * % java BinarySearch tinyW.txt < tinyT.txt
+ * 50
+ * 99
+ * 13
+ * <p>
+ * % java BinarySearch largeW.txt < largeT.txt | more
+ * 499569
+ * 984875
+ * 295754
+ * 207807
+ * 140925
+ * 161828
+ * [367,966 total values]
+ * <p>
+ * <p>
+ * 二分查找实现
+ */
+/**
+ * 二分查找实现
+ */
 package edu.princeton.cs.algs4;
 
 import java.util.Arrays;
 
 /**
- *  The {@code BinarySearch} class provides a static method for binary
- *  searching for an integer in a sorted array of integers.
- *  <p>
- *  The <em>indexOf</em> operations takes logarithmic time in the worst case.
- *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/11model">Section 1.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * The {@code BinarySearch} class provides a static method for binary
+ * searching for an integer in a sorted array of integers.
+ * <p>
+ * The <em>indexOf</em> operations takes logarithmic time in the worst case.
+ * <p>
+ * For additional documentation, see <a href="https://algs4.cs.princeton.edu/11model">Section 1.1</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
 public class BinarySearch {
 
     /**
      * This class should not be instantiated.
      */
-    private BinarySearch() { }
+    private BinarySearch() {
+    }
 
     /**
      * Returns the index of the specified key in the specified array.
      *
-     * @param  a the array of integers, must be sorted in ascending order
-     * @param  key the search key
+     * @param a   the array of integers, must be sorted in ascending order
+     * @param key the search key
      * @return index of key in array {@code a} if present; {@code -1} otherwise
      */
     public static int indexOf(int[] a, int key) {
@@ -59,7 +64,7 @@ public class BinarySearch {
         while (lo <= hi) {
             // Key is in a[lo..hi] or not present.
             int mid = lo + (hi - lo) / 2;
-            if      (key < a[mid]) hi = mid - 1;
+            if (key < a[mid]) hi = mid - 1;
             else if (key > a[mid]) lo = mid + 1;
             else return mid;
         }
@@ -71,8 +76,8 @@ public class BinarySearch {
      * This function is poorly named because it does not give the <em>rank</em>
      * if the array has duplicate keys or if the key is not in the array.
      *
-     * @param  key the search key
-     * @param  a the array of integers, must be sorted in ascending order
+     * @param key the search key
+     * @param a   the array of integers, must be sorted in ascending order
      * @return index of key in array {@code a} if present; {@code -1} otherwise
      * @deprecated Replaced by {@link #indexOf(int[], int)}.
      */
@@ -94,14 +99,15 @@ public class BinarySearch {
         In in = new In(args[0]);
         int[] allowlist = in.readAllInts();
 
-        // sort the array
+        // sort the array，先把数列按小到大排序了
         Arrays.sort(allowlist);
 
         // read integer key from standard input; print if not in allowlist
         while (!StdIn.isEmpty()) {
             int key = StdIn.readInt();
             if (BinarySearch.indexOf(allowlist, key) == -1)
-                StdOut.println(key);
+                StdOut.println(key);//没找到输出key
+            else System.out.println(BinarySearch.indexOf(allowlist, key));//找到输出下标
         }
     }
 }
